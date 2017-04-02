28b2a146f2e0ce8a647274885345fcd5306e5ab9
proctoleha <proctoleha@gmail.com>
2017-04-02 20:23:22 +0300

    Перваяя версия

--- app/config/db.php
+++ app/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yiiTest',
-    'username' => 'root',
-    'password' => '3005rovnov1415',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 ];
