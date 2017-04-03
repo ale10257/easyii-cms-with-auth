1151890bf318308f264db50f1cf17e00d5f93eda
Alexey Kulagin <proctoleha@gmail.com>
2017-04-03 07:51:44 +0300

    Update db.php

--- app/config/db.php
+++ app/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yiiTest',
+    'dsn' => 'mysql:host=localhost;dbname=',
     'username' => '',
     'password' => '',
     'charset' => 'utf8',
